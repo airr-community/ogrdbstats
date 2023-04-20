c7067c22b234f83493fb7f0a228b954ba11ddb19
William Lees <william@lees.org.uk>
2023-04-20 10:53:53 +0100

    Provide Docker container

---
+++ install_ogrdbstats.R
@@ -0,0 +1,6 @@
+# Install ogrdbstats - intended for use in building the Docker image
+
+library(devtools)
+devtools::install_github('https://github.com/airr-community/ogrdbstats', lib="/usr/share/R/library", upgrade="never")
+
+
